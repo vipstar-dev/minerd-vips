a6d637d2db7e4e5cbfce9bbf12a2f419d2c55c49
vipstar-dev <42050784+vipstar-dev@users.noreply.github.com>
2018-10-02 03:33:17 +0900

    fix build error

--- miner.h
+++ miner.h
@@ -160,8 +160,7 @@ extern int scanhash_yespower(int thr_id, uint32_t *pdata,
 extern int scanhash_sha256d(int thr_id, uint32_t *pdata,
 	const uint32_t *ptarget, uint32_t max_nonce, unsigned long *hashes_done);
 
-extern int scanhash_sha256d_vips(int thr_id, uint32_t *pdata,
-	const uint32_t *ptarget, uint32_t max_nonce, unsigned long *hashes_done);
+extern int scanhash_sha256d_vips(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done);
 
 extern unsigned char *scrypt_buffer_alloc(int N);
 extern int scanhash_scrypt(int thr_id, uint32_t *pdata,
