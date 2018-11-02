f7d46501da6f2801ab3b8a7611b668323672d8c6
vipstar-dev <42050784+vipstar-dev@users.noreply.github.com>
2018-11-02 13:54:49 +0900

    add algo vipstar

--- miner.h
+++ miner.h
@@ -160,6 +160,9 @@ extern int scanhash_yespower(int thr_id, uint32_t *pdata,
 extern int scanhash_sha256d(int thr_id, uint32_t *pdata,
 	const uint32_t *ptarget, uint32_t max_nonce, unsigned long *hashes_done);
 
+extern int scanhash_sha256d_vips(int thr_id, uint32_t *pdata,
+	const uint32_t *ptarget, uint32_t max_nonce, unsigned long *hashes_done);
+
 extern unsigned char *scrypt_buffer_alloc(int N);
 extern int scanhash_scrypt(int thr_id, uint32_t *pdata,
 	unsigned char *scratchbuf, const uint32_t *ptarget,
